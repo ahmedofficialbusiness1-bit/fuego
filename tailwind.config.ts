60c34df5c1d8f57b0ecf2ae9bb7b71e1154c5b8c
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-08-21 17:49:30 +0000

    hapana speed sio kubwa rejesha vile vile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -109,7 +109,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'move-dots': 'move-dots 2s linear infinite',
-        'disperse-and-gather': 'disperse-and-gather 15s ease-in-out infinite',
+        'disperse-and-gather': 'disperse-and-gather 5s ease-in-out infinite',
       },
     },
   },
