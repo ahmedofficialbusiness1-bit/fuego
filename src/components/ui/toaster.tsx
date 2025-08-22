569a2bec625568f5412d93160455cc4471b1271c
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-08-22 14:58:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -16,8 +16,9 @@ export function Toaster() {
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
+        const { onOpenChange, ...rest } = props;
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...rest}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
