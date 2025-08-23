5d2de456d2302b61972af62a6c5a016e31076a31
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-08-23 06:58:37 +0000

    sawa sasa hio card yake isogeze juu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -266,7 +266,7 @@ export default function Home() {
             <h2 className="text-4xl md:text-5xl font-headline font-extrabold text-foreground tracking-tighter">Sifa za Fuego</h2>
             <p className="max-w-xl text-muted-foreground mx-auto text-sm mt-4">Gundua sifa za kiufundi za Fuego SmartCook.</p>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-7xl w-full mx-auto items-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-7xl w-full mx-auto items-start">
             <div className="relative w-full h-[80vh]">
               <Image
                 src="/Adobe Express - file.png"
@@ -406,3 +406,5 @@ export default function Home() {
     </>
   );
 }
+
+    
