f18f8af3df1ccf94cad9e28cdd26a791f508b2fb
MAISHA YA SUPERSTAR <elonjazz89@gmail.com>
2025-08-23 07:00:25 +0000

    na picha pia ipandishe juu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -267,7 +267,7 @@ export default function Home() {
             <p className="max-w-xl text-muted-foreground mx-auto text-sm mt-4">Gundua sifa za kiufundi za Fuego SmartCook.</p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-7xl w-full mx-auto items-start">
-            <div className="relative w-full h-[80vh]">
+            <div className="relative w-full h-[70vh]">
               <Image
                 src="/Adobe Express - file.png"
                 alt="Fuego SmartCook on a counter"
